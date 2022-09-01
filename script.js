6997cd2daee178633191341181493ccfce691f0d
Einma <tetbirtaimen0@gmail.com>
2022-09-01 10:04:50 +0100

    create two color modes and buttons for each mode rainbow mode black mode

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const DEFAULT_SIZE = 8;
+const COLOR = 'black';
 
 
 const SLIDER = document.querySelector('#range');
 const CLEAR = document.querySelector('#clear');
 const INPUT_VALUE = document.querySelector('#input-value')
+const RAINBOW_MODE = document.querySelector('#rainbow');
+const BLACK_MODE = document.querySelector('#black');
 
 const CANVAS = document.querySelector('#canvas');
 const CELLS = document.querySelectorAll('#cell');
+
+
+let rainbow = false;
+let black = true;
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 fillGrid(DEFAULT_SIZE)
 coloring();
 
 
-CANVAS.childNodes.forEach(cell => {
-    cell.addEventListener('mouseover', event => {
-        cell.style = 'background-color: green'
-    })
-})
 
 function clearGrid() {
     CANVAS.innerHTML = ''; //Empties the grid
@@ -46,27 +53,37 @@ SLIDER.oninput = function () {
     coloring();
 }
 
+CLEAR.addEventListener('click', () => {
+    fillGrid(SLIDER.value);
+    coloring();
+})
+
+RAINBOW_MODE.addEventListener('click', () => {
+    rainbow = true;
+    black = false;
+    RAINBOW_MODE.classList.add('active');
+    BLACK_MODE.classList.remove('active');
+})
+BLACK_MODE.addEventListener('click', () => {
+    rainbow = false;
+    black = true;
+    RAINBOW_MODE.classList.remove('active');
+    BLACK_MODE.classList.add('active');
+})
 
 
 function coloring() {
     CANVAS.childNodes.forEach(cell => {
         cell.addEventListener('mouseover', event => {
-            cell.style = 'background-color: green'
+            if (rainbow === true && black === false) {
+                const randomR = Math.floor(Math.random() * 256)
+                const randomG = Math.floor(Math.random() * 256)
+                const randomB = Math.floor(Math.random() * 256)
+                cell.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+            } else if (rainbow === false && black === true) {
+                cell.style.backgroundColor = `${COLOR}`
+            }
         })
     })
 }
 
-// CANVAS.childNodes.forEach(cell => {
-//     cell.addEventListener('mouseover', event => {
-//         cell.style = 'background-color: green'
-//     })
-// })
-
-
-
-
-// CELLS.forEach(item => {
-//     item.addEventListener('click', event => {
-//         console.log('hre');
-//     })
-// })
